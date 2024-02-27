835a0936495824e8134ffb6c3c5f7c25e759c843
Minh-Tri Pham <pmtri80@gmail.com>
2024-02-27 15:00:45 +0000

    (bug-fix commit by MT)

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup, find_namespace_packages
-from mt.glm.version import version
+
+VERSION_FILE = os.path.join(os.path.dirname(__file__), "VERSION.txt")
 
 setup(
     name="mtglm",
-    version=version,
     description="Minh-Tri Pham's extra modules for dealing with GLM in Python",
     author=["Minh-Tri Pham"],
     packages=find_namespace_packages(include=["mt.*"]),
@@ -16,4 +17,13 @@ setup(
         "pyglm",
         # "mtbase>=4.25",  # we don't need mtbase yet
     ],
+    setup_requires=["setuptools-git-versioning<2"],
+    setuptools_git_versioning={
+        "enabled": True,
+        "version_file": VERSION_FILE,
+        "count_commits_from_version_file": True,
+        "template": "{tag}",
+        "dev_template": "{tag}.dev{ccount}+{branch}",
+        "dirty_template": "{tag}.post{ccount}",
+    },
 )
